d1ca94f37d996a7657488f40001e6756cf0e8320
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-02-04 23:44:58 +0100

    add attempt results

--- app.rb
+++ app.rb
@@ -9,12 +9,24 @@ def some_method(first_argument, second_argument, third_argument, *other_stuff)
 end
 
 some_method('I', 'AM', 'GROOT', '!', '!')
+# => 'I'
+# => 'AM'
+# => 'GROOT'
+# => ['!', '!']
 
 # Splat to unpack array
 arr = ['I', 'AM', 'GROOT', '!']
 some_method(*arr)
+# => 'I'
+# => 'AM'
+# => 'GROOT'
+# => ['!']
 
 # Double splat to combine hash
 hsh = { stuff: 'and thinks' }
 hsh2 = { yes: 'yes' }
 some_method('I', 'AM', 'GROOT', **hsh, **hsh2)
+# => 'I'
+# => 'AM'
+# => 'GROOT'
+# => '[{stuff: 'and thinks', yes: 'yes}]'
